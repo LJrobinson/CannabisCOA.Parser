aaf92d5dde50e1bc16d2a985f9e16d65fdc19fbc
LJ <wash3d@gmail.com>
2026-04-25 18:13:48 -0700

    Add harvest and package date coverage

--- tests/CannabisCOA.Parser.Core.Tests/CoaParserDateTests.cs
+++ tests/CannabisCOA.Parser.Core.Tests/CoaParserDateTests.cs
@@ -56,7 +56,61 @@ public class CoaParserDateTests
     }
 
     [Fact]
-    public void Does_Not_Use_HarvestDate_As_TestDate()
+    public void Extracts_HarvestDate_FromHarvestLabel()
+    {
+        var text = """
+        THC: 1.0%
+        THCA: 20.0%
+        Harvest Date: 03/10/2026
+        Test Date: 04/01/2026
+        """;
+
+        var result = CoaParser.Parse(text);
+
+        Assert.NotNull(result.HarvestDate);
+        Assert.Equal(2026, result.HarvestDate!.Value.Year);
+        Assert.Equal(3, result.HarvestDate.Value.Month);
+        Assert.Equal(10, result.HarvestDate.Value.Day);
+    }
+
+    [Fact]
+    public void Extracts_PackageDate_FromPackageLabel()
+    {
+        var text = """
+        THC: 1.0%
+        THCA: 20.0%
+        Package Date: 03/20/2026
+        Test Date: 04/01/2026
+        """;
+
+        var result = CoaParser.Parse(text);
+
+        Assert.NotNull(result.PackageDate);
+        Assert.Equal(2026, result.PackageDate!.Value.Year);
+        Assert.Equal(3, result.PackageDate.Value.Month);
+        Assert.Equal(20, result.PackageDate.Value.Day);
+    }
+
+    [Fact]
+    public void Extracts_All_Date_Types_Independently()
+    {
+        var text = """
+        THC: 1.0%
+        THCA: 20.0%
+        Harvest Date: 2026-03-10
+        Package Date: 2026-03-20
+        Test Date: 2026-04-01
+        """;
+
+        var result = CoaParser.Parse(text);
+
+        Assert.Equal(new DateTime(2026, 3, 10), result.HarvestDate);
+        Assert.Equal(new DateTime(2026, 3, 20), result.PackageDate);
+        Assert.Equal(new DateTime(2026, 4, 1), result.TestDate);
+    }
+
+    [Fact]
+    public void Does_Not_Use_HarvestDate_Or_PackageDate_As_TestDate()
     {
         var text = """
         THC: 1.0%
@@ -68,6 +122,8 @@ public class CoaParserDateTests
         var result = CoaParser.Parse(text);
 
         Assert.Null(result.TestDate);
+        Assert.Equal(new DateTime(2024, 1, 1), result.HarvestDate);
+        Assert.Equal(new DateTime(2024, 2, 1), result.PackageDate);
     }
 
     [Fact]
